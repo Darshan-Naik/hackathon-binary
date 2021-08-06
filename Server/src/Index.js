d2a161cb15b072a314ab6327e8ae0493b086be23
SOUMYADRI DAS <dadsoumyadri@gmail.com>
2021-08-06 23:53:16 +0530

    SocketIO conection build

--- Server/src/Index.js
+++ Server/src/Index.js
@@ -1,5 +1,31 @@
 const app = require("./server.js");
-const connect = require("./Config/db")
+
+const http = require("http");
+const connect = require("./Config/db");
+
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+})
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
 
 const start = async () => {
     await connect();
--- Server/src/Model/category.model.js
+++ Server/src/Model/category.model.js
@@ -5,7 +5,8 @@ const categoryModel = new Schema(
         name: {type: String, require: true},
         growth: {type: String, require: true},
         icon: {type: String, require: true},
-        popularity: {type: Number, require: true}
+        popularity: {type: Number, require: true},
+        category: {type: String, require: true}
     },
     {
         versionKey: false,
