09f126a65ebb4399205bb4563f4457f19fe99e5b
darshan <darshu8314@gmail.com>
2021-08-06 17:21:10 +0530

    homepage UI updated

--- Client/src/Components/Navbar/Navbar.jsx
+++ Client/src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import "../../Styles/Navbar/Navbar.css"
+import React from "react";
+import "../../Styles/Navbar/Navbar.css";
 
 function Navbar() {
-    return (
-      <nav className="flex nav-container">
-        <div className="logo-container">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5VOIJTKhQNgBLepC2ZrFqrEJ4s8fKh0FySbaKu-kQk9IHaRPOo8h6nQ6oglSQX4GxciM&usqp=CAU"
-            alt="logo"
-          />
-        </div>
-        <div className="nav-menu-container">
-            <h1>Menu</h1>
-        </div>
-        <div className="login-button-container">
-            <button>Login</button>
-        </div>
-      </nav>
-    );
+  return (
+    <nav className="flex nav-container">
+      <div className="logo-container">
+        <img src={process.env.PUBLIC_URL + "/Images/Logo.png"} alt="logo" />
+      </div>
+      <div className="nav-menu-container">
+        <h1>Menu</h1>
+      </div>
+      <div className="login-button-container">
+        <button>Login</button>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
