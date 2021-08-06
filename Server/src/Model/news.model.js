875a4308cc0a21c4b9c9ee503432a294886233e8
Guru-Eco <abhimanyu768692@gmail.com>
2021-08-06 21:29:08 +0530

    backend news collection added

--- Server/src/Model/news.model.js
+++ Server/src/Model/news.model.js
@@ -4,9 +4,7 @@ const newsSchema = Schema(
     {
         title: { type: String, required: true },
         body: { type: String, required: true },
-        category: [
-            { type: String, required: true }
-        ],
+        category: { type: String, required: true },
         dateTime: { type: String, required: true },
         imageUrl: { type: String }
     }
