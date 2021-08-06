f15f0fc8784f9658a54cc40e27dcebde67ad0b71
darshan <darshu8314@gmail.com>
2021-08-06 16:56:41 +0530

    homepage UI added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Home />
     </div>
   );
 }
