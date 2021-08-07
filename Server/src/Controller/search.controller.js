6a7033b9ac773fab4506a4f233c43254dc4ea82a
Guru-Eco <abhimanyu768692@gmail.com>
2021-08-07 12:27:04 +0530

    backend added appointment collection

--- Server/src/Controller/search.controller.js
+++ Server/src/Controller/search.controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Article = require('../Model/article.model');
 const Blog = require('../Model/blog.model');
+const Mentor = require('../Model/mentor.model');
 const News = require('../Model/news.model');
 const router = express.Router();
 
@@ -8,7 +9,7 @@ router.get('/', async (req, res) => {
     try {
         let query = req.query.q;
 
-        let blogs = await Blog.find({ "category": { $eq: query.toLowerCase() } }).populate('mentors').sort({ "createdAt": -1 }).lean().exec();
+        let blogs = await Blog.find({ "category": { $eq: query.toLowerCase() } }).populate('authorId').sort({ "createdAt": -1 }).lean().exec();
         let articles = await Article.find({ "category": query.toLowerCase() }).sort({ "createdAt": -1 }).lean().exec();
         let news = await News.find({ "category": query.toLowerCase() }).sort({ "createdAt": -1 }).lean().exec();
 
@@ -17,5 +18,19 @@ router.get('/', async (req, res) => {
         return res.status(500).json({ status: "Failed", message: "Something went wrong." })
     }
 });
+
+router.get('/mentors', async (req, res) => {
+    try {
+
+        let query = req.query.q;
+
+        let mentors = await Mentor.find({ specialization: query.toLowerCase() }).lean().exec();
+
+        return res.status(200).json({ data: mentors });
+
+    } catch (error) {
+        return res.status(500).json({ status: "Failed", message: "Something went wrong." });
+    }
+})
 
 module.exports = router;
