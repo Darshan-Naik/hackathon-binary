4d0b7afcd72655eeebc7e4cabce257d81a407115
Guru-Eco <abhimanyu768692@gmail.com>
2021-08-18 14:56:48 +0530

    uri

--- Server/src/Config/db.js
+++ Server/src/Config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    return mongoose.connect(`${process.env.SECRET_URL}`, {
+    return mongoose.connect('mongodb+srv://darshan:darshan@cluster0.hv4pz.mongodb.net/binary?retryWrites=true&w=majority', {
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
